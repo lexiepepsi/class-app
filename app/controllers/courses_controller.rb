e48422a8cf4db69a168c247788ee0da69ce29a52
alexabark <alexa@barkbox.com>
2024-02-20 22:14:25 -0500

    add courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,17 @@
 class CoursesController < ApplicationController
 	def index
+		# ENV['TEACHABLE_API_KEY'] ||
+		# TEACHABLE_API_KEY = '7JbSA3ep6XOMV3t8t7QXuXq9HS79Dwnr'
+		url = URI("https://developers.teachable.com/v1/courses")
+
+		http = Net::HTTP.new(url.host, url.port)
+		http.use_ssl = true
+
+		request = Net::HTTP::Get.new(url, {'apiKey': '7JbSA3ep6XOMV3t8t7QXuXq9HS79Dwnr'})
+		request["accept"] = 'application/json'
+
+		response = http.request(request)
+		# puts response.read_body
+		@results = JSON.parse(response.body, symbolize_names: true)
 	end
 end
